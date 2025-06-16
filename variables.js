5d3a937d346d6d1142b89291d81d2d8b7a2b97be
altamashjamadar <iamaltamashjamadar@gmail.com>
2025-06-16 17:08:33 +0530

    first commit

---
+++ variables.js
@@ -0,0 +1,17 @@
+const accountId=425212
+let accountEmail ="aj@google.com"
+var accountPassword ="123321"
+accountCity="Pune"
+let accountState;
+
+// accountId=211
+ //Cant reassign const variable
+console.log(accountId);
+console.log(accountEmail);
+
+
+accountEmail="hc@hc.com"
+accountPassword="123456"
+accountCity="Mumbai"
+
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState])
