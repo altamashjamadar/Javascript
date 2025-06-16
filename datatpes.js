eac93e788d93d6160a1c46406bfff7500e9e26f7
altamashjamadar <iamaltamashjamadar@gmail.com>
2025-06-16 17:18:20 +0530

    datatypes

---
+++ datatpes.js
@@ -0,0 +1,34 @@
+let name="Ajay"
+let age=25
+let isEmployed=true 
+let salary=50000.50
+// let hobbies=["reading", "gaming", "coding"]
+
+console.log(typeof name); // string
+console.log(typeof age); // number  
+console.log(typeof isEmployed); // boolean
+console.log(typeof salary); // number
+console.log(typeof hobbies); // object (arrays are a type of object in JavaScript)
+console.log(typeof null); // object (this is a known quirk in JavaScript)
+console.log(typeof undefined); // undefined
+
+
+//number = 2 to power of 53
+// BigInt
+//String=>""
+//Boolean => true/false
+//Symbol => unique identifier
+//null => standalone value representing "no value" or "empty"
+//undefined => variable not assigned
+
+/* go through specification of data types in JavaScript
+ * 1. Number
+ * 2. String
+ * 3. Boolean
+ * 4. BigInt
+ * 5. Symbol
+ * 6. null
+ * 7. undefined
+ * 8. Object (including arrays, functions, etc.)
+ * ecmascript data types
+ */
